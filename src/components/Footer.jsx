f9cdd8549f0b59df25de00cdd33273813c50540c
H-JUYEONG <dud9902@gmail.com>
2025-04-20 16:18:23 +0900

    사이트명 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
 
         <div className={styles.infoContainer}>
           <div className={styles.logoContainer}>
-            <h1 className={styles.logo}>Dear</h1>
+            <h1 className={styles.logo}>IDEA</h1>
           </div>
 
           <div className={styles.info}>
@@ -43,19 +43,19 @@ const Footer = () => {
               사용자 고객센터 02-6226-5000 (평일 09:00~19:00, 주말·공휴일 휴무)
             </p>
             <p className={styles.infoText}>
-              이메일 : help@saramin.co.kr, Fax : 02-6937-0039(대표),
+              이메일 : help@idea.co.kr, Fax : 02-6937-0039(대표),
               02-6937-0035(세금계산서)
             </p>
             <p className={styles.infoText}>
-              (주)사람인, 우 : 07800, 서울특별시 강서구 공항대로 165, 캐노츠랜드
-              11층, 대표 : 윤병호
+              (주)아이디어, 우 : 07800, 서울특별시 강서구 공항대로 165, 캐노츠랜드
+              11층, 대표 : 임진황
             </p>
             <p className={styles.infoText}>
               사업자등록 : 113-86-00917, 직업정보제공사업 : 서울 관악 제
               2005-6호, 통신판매업 : 제 2025-서울강서-0847호
             </p>
             <p className={styles.infoText}>
-              Copyright (c) (주)사람인. All rights reserved.
+              Copyright (c) (주)아이디어. All rights reserved.
             </p>
           </div>
 
