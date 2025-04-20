7b45af92e94f87e00de85a10a9ff29fc1e64fe35
H-JUYEONG <dud9902@gmail.com>
2025-04-20 15:20:10 +0900

    header 추가

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../assets/css/Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={`${styles.container} ${styles.headerContainer}`}>
+        <Link to="/" className={styles.logo}>
+          Dear
+        </Link>
+        <div className={styles.headerRight}>
+          <button className={styles.profileButton}>
+            <div className={styles.profileImage}>홍</div>
+            <span>홍길동님</span>
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
