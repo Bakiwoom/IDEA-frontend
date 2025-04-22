86ea5350b775a9e017e156df9bd3feeab1c0e566
soyoungJin44 <wlsthdud0412@naver.com>
2025-04-23 08:22:23 +0900

    카테고리 화면 ui 개발

--- src/pages/user/CategotyListPage.jsx
+++ src/pages/user/CategotyListPage.jsx
@@ -20,9 +20,59 @@ const CategoryListPage = () =>{
                 </ul>
             </div>
             <div className={styles.container}>
+                <h2 className={styles.pageTitle}>장애인 채용공고</h2>
                 <div className={styles.itemBoxContainer}>
                     <div className={styles.itemBox}>
-                        내용
+                        <img src="http://localhost:3000/images/%EC%82%BC%EC%84%B1%EB%A1%9C%EA%B3%A0.png" className={styles.logoImg} alt="업체로고"></img>
+                        <h3 className={styles.itemTitle}>[쿠팡 CFS] 지역허브 셀러보상 전문가 림핑크룸 정규직 채용</h3>
+                        <p className={styles.itemCompany}>쿠팡풀필먼트서비스(주)</p>
+                        <div className={styles.spanBox}>
+                            <span><i>📍</i>경기/인천</span>
+                            <span><i>🏢</i>신입/경력</span>
+                        </div>
+                        <div className={styles.dayBox}>
+                            <span className={styles.itemEndday}>~05/20</span>
+                            <span className={styles.heartIcon}>♡</span>
+                        </div>
+                    </div>
+                    <div className={styles.itemBox}>
+                        <img src="http://localhost:3000/images/%EC%82%BC%EC%84%B1%EB%A1%9C%EA%B3%A0.png" className={styles.logoImg} alt="업체로고"></img>
+                        <h3 className={styles.itemTitle}>[쿠팡 CFS] 지역허브 셀러보상 전문가 림핑크룸 정규직 채용</h3>
+                        <p className={styles.itemCompany}>쿠팡풀필먼트서비스(주)</p>
+                        <div className={styles.spanBox}>
+                            <span><i>📍</i>경기/인천</span>
+                            <span><i>🏢</i>신입/경력</span>
+                        </div>
+                        <div className={styles.dayBox}>
+                            <span className={styles.itemEndday}>~05/20</span>
+                            <span className={styles.heartIcon}>♡</span>
+                        </div>
+                    </div>
+                    <div className={styles.itemBox}>
+                        <img src="http://localhost:3000/images/%EC%82%BC%EC%84%B1%EB%A1%9C%EA%B3%A0.png" className={styles.logoImg} alt="업체로고"></img>
+                        <h3 className={styles.itemTitle}>[쿠팡 CFS] 지역허브 셀러보상 전문가 림핑크룸 정규직 채용</h3>
+                        <p className={styles.itemCompany}>쿠팡풀필먼트서비스(주)</p>
+                        <div className={styles.spanBox}>
+                            <span><i>📍</i>경기/인천</span>
+                            <span><i>🏢</i>신입/경력</span>
+                        </div>
+                        <div className={styles.dayBox}>
+                            <span className={styles.itemEndday}>~05/20</span>
+                            <span className={styles.heartIcon}>♡</span>
+                        </div>
+                    </div>
+                    <div className={styles.itemBox}>
+                        <img src="http://localhost:3000/images/%EC%82%BC%EC%84%B1%EB%A1%9C%EA%B3%A0.png" className={styles.logoImg} alt="업체로고"></img>
+                        <h3 className={styles.itemTitle}>[쿠팡 CFS] 지역허브 셀러보상 전문가 림핑크룸 정규직 채용</h3>
+                        <p className={styles.itemCompany}>쿠팡풀필먼트서비스(주)</p>
+                        <div className={styles.spanBox}>
+                            <span><i>📍</i>경기/인천</span>
+                            <span><i>🏢</i>신입/경력</span>
+                        </div>
+                        <div className={styles.dayBox}>
+                            <span className={styles.itemEndday}>~05/20</span>
+                            <span className={styles.heartIcon}>♡</span>
+                        </div>
                     </div>
                 </div>
                 
