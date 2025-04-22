2a09828841587cd9121da064a9e4f3e9e3cc1764
soyoungJin44 <wlsthdud0412@naver.com>
2025-04-23 08:28:33 +0900

    메인화면 채용정보 연결LINK주소 수정

--- src/routes/contantsRoutes.js
+++ src/routes/contantsRoutes.js
@@ -4,3 +4,4 @@ export const LOGIN_PAGE = '/user/loginPage';
 export const SIGNUP_PAGE = '/user/signUpTypeChoicePage';
 
 export const USER_MYPAGE_MAIN = 'user/mypage/userMyPageMain';
+export const CATEGORY_PAGE = 'user/categoryListPage';
