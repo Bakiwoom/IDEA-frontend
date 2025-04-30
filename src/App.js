b60ddd5c842441a367df7011e81780661081e826
H-JUYEONG <https://github.com/H-JUYEONG>
2025-04-30 15:45:38 +0900

    vendor를 company로 수정정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/main/Main.jsx";
 import UserRoutes from "./routes/UserRoutes";
-import VendorRoutes from "./routes/VendorRoutes";
+import CompanyRoutes from "./routes/CompanyRoutes.jsx";
 import AdminRoutes from "./routes/AdminRoutes";
 import Layout from "./components/Layout";
-import VendorLayout from "./components/VendorLayout";
+import CompanyLayout from "./components/CompanyLayout.jsx";
 
 import "./assets/css/all.css";
 
@@ -13,8 +13,8 @@ function App() {
     <Router>
       <Routes>
         {/* 푸터가 필요 없는 벤더 경로 */}
-        <Route element={<VendorLayout />}>
-          <Route path="/vendor/*" element={<VendorRoutes />} />
+        <Route element={<CompanyLayout />}>
+          <Route path="/company/*" element={<CompanyRoutes />} />
         </Route>
 
         {/* 푸터가 있는 나머지 경로 */}
--- src/pages/vendor/ApplicantManagement.jsx
+++ src/pages/vendor/ApplicantManagement.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import styles from "../../assets/css/vendor/ApplicantManagement.module.css";
-import VendorSidebar from "./VendorSidebar";
+import VendorSidebar from "./CompanySidebar";
 
 const ApplicantManagement = () => {
   const [activeMenu, setActiveMenu] = useState("applicant-management");
