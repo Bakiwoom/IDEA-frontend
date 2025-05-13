7405d80f51a6013bebb2f1178dadcbe0aa812f95
H-JUYEONG <https://github.com/H-JUYEONG>
2025-05-13 17:07:40 +0900

    채용공고 화면에서 로고없을 경우 이미지 변경 추가가

--- src/pages/user/CategotyListPage.jsx
+++ src/pages/user/CategotyListPage.jsx
@@ -5,12 +5,12 @@ import { useNavigate } from "react-router-dom";
 import styles from "../../assets/css/user/CategotyListPage.module.css";
 import Nav from "../../components/Navbar";
 
-import {useAuth} from "../../contexts/user/AuthProvider";
+import { useAuth } from "../../contexts/user/AuthProvider";
 
 
-const CategoryListPage = () =>{
+const CategoryListPage = () => {
 
-    const {authUser, memberId, role, userId} = useAuth();
+    const { authUser, memberId, role, userId } = useAuth();
     const navigate = useNavigate();
 
     const apiUrl = process.env.REACT_APP_API_URL;
@@ -38,31 +38,31 @@ const CategoryListPage = () =>{
     };
 
 
-    const handleClickCategory = (item) =>{
+    const handleClickCategory = (item) => {
         setSelectedCategory(item);
     }
 
     //공고글 가져오기
-    const getList = ()=>{
+    const getList = () => {
         axios({
             method: "get",
             url: `${apiUrl}/api/disability`,
             headers: { "Content-Type": "application/json; charset=utf-8" },
             responseType: "json",
-            
-          })
+
+        })
             .then((response) => {
-              setListVo(response.data.apiData);
+                setListVo(response.data.apiData);
             })
             .catch((error) => {
-              console.error("disabilityPage리스트 가져오기 실패:", error);
-              alert("서버와의 연결 중 문제가 발생했습니다. 다시 시도해 주세요.");
+                console.error("disabilityPage리스트 가져오기 실패:", error);
+                alert("서버와의 연결 중 문제가 발생했습니다. 다시 시도해 주세요.");
             });
     };
 
     //북마크 리스트 가져오기
-    const getBookmarkList = ()=>{
-        
+    const getBookmarkList = () => {
+
         if (!userId || userId === '') return;
 
         axios({
@@ -70,26 +70,26 @@ const CategoryListPage = () =>{
             url: `${apiUrl}/api/disability/bookmarkList/${userId}`,
             headers: { "Content-Type": "application/json; charset=utf-8" },
             responseType: "json",
-            
-          })
+
+        })
             .then((response) => {
-              setBookmarkList(response.data.apiData);
+                setBookmarkList(response.data.apiData);
             })
             .catch((error) => {
-              console.error("bookmarkList 가져오기 실패:", error);
-              alert("서버와의 연결 중 문제가 발생했습니다. 다시 시도해 주세요.");
+                console.error("bookmarkList 가져오기 실패:", error);
+                alert("서버와의 연결 중 문제가 발생했습니다. 다시 시도해 주세요.");
             });
     }
 
     //좋아요 등록
-    const handleHeart = (jobId)=>{
-        
-        if(role === 'company'){
+    const handleHeart = (jobId) => {
+
+        if (role === 'company') {
             alert('기업회원은 사용불가능한 기능입니다.');
             return;
         }
 
-        if(!memberId){
+        if (!memberId) {
             alert('로그인이 필요한 기능입니다.')
             return;
         }
@@ -97,32 +97,32 @@ const CategoryListPage = () =>{
         const isBookmarked = bookmarkList.some(bookmark => bookmark.jobId === jobId);
 
         //삭제
-        if(isBookmarked){
+        if (isBookmarked) {
             axios({
                 method: "delete",
                 url: `${apiUrl}/api/disability/bookmark/${jobId}/${userId}`,
                 headers: { "Content-Type": "application/json; charset=utf-8" },
                 responseType: "json",
             })
-            .then(() => {
-                // 북마크 리스트에서 해당 공고 제거
-                setBookmarkList((prevBookmarkList) => 
-                    prevBookmarkList.filter(bookmark => bookmark.jobId !== jobId)
-                );
-            })
-            .catch((error) => {
-                console.error("북마크 삭제 실패:", error);
-                alert("서버와의 연결 중 문제가 발생했습니다. 다시 시도해 주세요.");
-            });
-        //등록
-        }else{
+                .then(() => {
+                    // 북마크 리스트에서 해당 공고 제거
+                    setBookmarkList((prevBookmarkList) =>
+                        prevBookmarkList.filter(bookmark => bookmark.jobId !== jobId)
+                    );
+                })
+                .catch((error) => {
+                    console.error("북마크 삭제 실패:", error);
+                    alert("서버와의 연결 중 문제가 발생했습니다. 다시 시도해 주세요.");
+                });
+            //등록
+        } else {
             axios({
                 method: "post",
                 url: `${apiUrl}/api/disability/bookmark/${jobId}/${userId}`,
                 headers: { "Content-Type": "application/json; charset=utf-8" },
                 responseType: "json",
-                
-              })
+
+            })
                 .then((response) => {
                     setBookmarkList((prevBookmarkList) => {
                         // 기존 북마크 리스트에 새로 등록된 jobId를 추가
@@ -131,24 +131,24 @@ const CategoryListPage = () =>{
                     });
                 })
                 .catch((error) => {
-                  console.error("북마크 등록 실패:", error);
-                  alert("서버와의 연결 중 문제가 발생했습니다. 다시 시도해 주세요.");
+                    console.error("북마크 등록 실패:", error);
+                    alert("서버와의 연결 중 문제가 발생했습니다. 다시 시도해 주세요.");
                 });
-            }
+        }
     }
 
 
-    useEffect(()=>{
+    useEffect(() => {
 
         //공고글 list가져오기
         getList();
         //북마크글 가져오기
         getBookmarkList();
 
-    },[]);
+    }, []);
 
 
-    return(
+    return (
         <>
             <Nav />
             {/* <div className={styles.categotyContainer}>
@@ -159,9 +159,13 @@ const CategoryListPage = () =>{
             <div className={styles.container}>
                 <h2 className={styles.pageTitle}>장애인 채용공고</h2>
                 <div className={styles.itemBoxContainer}>
-                    {listVo.map((item, index)=>(
-                        <div className={styles.itemBox} key={item.jobId} onClick={() => navigate(`/company/job/management/detail/${item.jobId}`)} style={{cursor:'pointer'}}>
-                            <img src={item.companyLogo} className={styles.logoImg} alt="업체로고"></img>
+                    {listVo.map((item, index) => (
+                        <div className={styles.itemBox} key={item.jobId} onClick={() => navigate(`/company/job/management/detail/${item.jobId}`)} style={{ cursor: 'pointer' }}>
+                            <img
+                                src={item.companyLogo || "/images/nologo.png"}
+                                className={styles.logoImg}
+                                alt="업체로고"
+                            ></img>
                             <h3 className={styles.itemTitle}>{item.title}</h3>
                             <p className={styles.itemCompany}>{item.companyName}</p>
                             <div className={styles.spanBox}>
@@ -171,22 +175,22 @@ const CategoryListPage = () =>{
                             </div>
                             <div className={styles.dayBox}>
                                 <span className={styles.itemEndday}>~ {item.deadline.slice(5)}</span>
-                                <span className={`${styles.heartIcon} ${role === 'company' ? '' : styles.iconHover} ${bookmarkList.some(bookmark => bookmark.jobId === item.jobId) ? styles.redHeart : ''}`}  onClick={(e)=>{e.stopPropagation();handleHeart(item.jobId);}}>
+                                <span className={`${styles.heartIcon} ${role === 'company' ? '' : styles.iconHover} ${bookmarkList.some(bookmark => bookmark.jobId === item.jobId) ? styles.redHeart : ''}`} onClick={(e) => { e.stopPropagation(); handleHeart(item.jobId); }}>
                                     {bookmarkList.some(bookmark => bookmark.jobId === item.jobId)
-                                    ? '♥' : '♡'
+                                        ? '♥' : '♡'
                                     }
                                 </span>
 
                             </div>
                         </div>
                     ))}
-                    
+
                 </div>
-                
+
             </div>
             <div className={styles.pagination}>
                 {pageNumbers.map((number) => (
-                    <button 
+                    <button
                         key={number}
                         onClick={() => handlePageChange(number)}
                         className={currentPage === number ? styles.activePage : ''}
